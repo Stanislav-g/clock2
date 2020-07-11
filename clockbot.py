4dd7c02a4ade583fc7b25aff2b9e41bbb3486d37
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-07-11 15:58:03 +0200

    Update clockbot.py

--- clockbot.py
+++ clockbot.py
@@ -76,18 +76,26 @@ async def clock(ctx, now, time ):
                 await asyncio.sleep(1)
                 await ctx.author.send(f'Вставай')
         else:
-            
-            sleeptimesec = sleeptime *60#минуты
-            await asyncio.sleep(sleeptimesec)
-            await ctx.author.send(f'Вставай')
-            await asyncio.sleep(1)
-            await ctx.author.send(f'Вставай')
-            await asyncio.sleep(1)
-            await ctx.author.send(f'Вставай')
-            await asyncio.sleep(1)
-            await ctx.author.send(f'Вставай')
-            await asyncio.sleep(1)
-            await ctx.author.send(f'Вставай')
+            f = nowtime
+            ff = f[0], f[1]
+            s = getup
+            ss = f[0], f[1]
+            if ff > ss and < 50:
+                first = sleeptime - 40
+                awaait asyncio.sleep(first)
+                await ctx.author.send(f'hihihihi')
+            else:
+                sleeptimesec = sleeptime *60#минуты
+                await asyncio.sleep(sleeptimesec)
+                await ctx.author.send(f'Вставай')
+                await asyncio.sleep(1)
+                await ctx.author.send(f'Вставай')
+                await asyncio.sleep(1)
+                await ctx.author.send(f'Вставай')
+                await asyncio.sleep(1)
+                await ctx.author.send(f'Вставай')
+                await asyncio.sleep(1)
+                await ctx.author.send(f'Вставай')
     else:
         sleeptime = float(day - nowtime + getup)
        
