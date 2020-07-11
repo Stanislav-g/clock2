53e05af3d46b997e64b3f7c1a5f9263bb9ee6fbe
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-07-11 19:25:58 +0200

    Update clockbot.py

--- clockbot.py
+++ clockbot.py
@@ -5,9 +5,9 @@ from discord.utils import get
 import asyncio
 from time import sleep
 from colorsys import hls_to_rgb
-import youtube_dl
+
 import os
-import requests
+
 import random
 from random import randint, choice, choices
 import io
@@ -31,8 +31,25 @@ async def on_redy():
 
 
 
+@client.command()
+async def status(ctx):
+    await ctx.channel.purge( limit = 1 )
+    while True:
+        await client.change_presence(activity=discord.Game(name='!help'))
+        await asyncio.sleep(60)
 
-
+        
+#help
+@client.command(pass_context = True)
+async def help(ctx):
+    await ctx.channel.purge(limit = 1)
+    emb = discord.Embed( 
+        title = 'Навигация по командам :clipboard:',
+        color = 0x7aa13d
+     )
+    
+    emb.add_field( name = '__**Информация**__', value = '''
+        ***!clock*** - будильник. Пример: !clock 1634 1635 !clock (время сейчас) (время вставать) 
 
 @client.command()
 async def clock(ctx, now, time ):
