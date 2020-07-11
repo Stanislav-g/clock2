500e5ea65c1a492399b0eec052baaf94363fe92a
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-07-11 16:01:11 +0200

    Update clockbot.py

--- clockbot.py
+++ clockbot.py
@@ -80,7 +80,7 @@ async def clock(ctx, now, time ):
             ff = f[0], f[1]
             s = getup
             ss = f[0], f[1]
-            if ff > ss and < 50:
+            if ff > ss and ss < 50:
                 first = sleeptime - 40
                 awaait asyncio.sleep(first)
                 await ctx.author.send(f'hihihihi')
