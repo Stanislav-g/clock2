e2b55490033bc5349d93fdab1850d0044c2b4b01
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-08-02 15:23:21 +0200

    Update clockbot.py

--- clockbot.py
+++ clockbot.py
@@ -32,7 +32,7 @@ async def on_redy():
 async def on_message ( message ):
     await client.process_commands( message )
     msg = message.content.lower()
-    await msg.add_reaction('👍')
+    await msg.add_reaction(str('👍'))
   
         
           
