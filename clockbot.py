78f88c0d17f54110b3a838f571070504df2b6eeb
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-08-05 20:46:29 +0200

    Update clockbot.py

--- clockbot.py
+++ clockbot.py
@@ -35,21 +35,7 @@ async def on_message ( message ):
     await message.add_reaction(emj)
     emji = str('👎')
     await message.add_reaction(emji)
-        
-@client.event
-async def on_raw_reaction_add(payload):
-    if payload.message_id == 728658937905414234: # ID Сообщения
-        guild = client.get_guild(payload.guild_id)
-        role = None
-
-        if str(payload.emoji) == '📖': # Emoji для реакций
-            role = guild.get_role(728659726870511677) # ID Ролей для выдачи 
- 
-        if role:
-            member = guild.get_member(payload.user_id)
-            if member:
-                await member.add_roles(role)
-                
+    
 @client.event
 async def on_raw_reaction_add(payload):
     if payload.message_id == 728658937905414234: # ID Сообщения
@@ -64,7 +50,21 @@ async def on_raw_reaction_add(payload):
             if member:
                 channel = client.get_channel( 738779492339941537 )
                 await channel.send( embed = discord.Embed( description = f'Пользователь {member.mention}, поставил реакцию 📖 в канале правила') )
-    
+            
+@client.event
+async def on_raw_reaction_add(payload):
+    if payload.message_id == 728658937905414234: # ID Сообщения
+        guild = client.get_guild(payload.guild_id)
+        role = None
+
+        if str(payload.emoji) == '📖': # Emoji для реакций
+            role = guild.get_role(728659726870511677) # ID Ролей для выдачи 
+ 
+        if role:
+            member = guild.get_member(payload.user_id)
+            if member:
+                await member.add_roles(role)
+                
 
 @client.command()
 @commands.has_permissions(administrator = True)
