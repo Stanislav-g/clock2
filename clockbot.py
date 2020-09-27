08f1032f89fa7a15b527ed3df36e1d149a88c533
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-09-27 17:43:06 +0200

    Update clockbot.py
    :good:

--- clockbot.py
+++ clockbot.py
@@ -43,9 +43,9 @@ async def on_member_join( member ):
    
 @client.event
 async def on_message ( message ):
-    emj = str(':good:')
+    emj = str('✅')
     await message.add_reaction(emj)
-    emji = str(':bad:')
+    emji = str('❌')
     await message.add_reaction(emji)
     
     
